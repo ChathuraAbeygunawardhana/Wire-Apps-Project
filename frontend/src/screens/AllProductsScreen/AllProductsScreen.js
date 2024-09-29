b181035329c21e9747d8d5ccfd511b8409e7131e
ChathuraAbeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-09-29 20:25:43 +0530

    WAP | Extracted the sortModal

--- frontend/src/screens/AllProductsScreen/AllProductsScreen.js
+++ frontend/src/screens/AllProductsScreen/AllProductsScreen.js
@@ -17,6 +17,7 @@ import renderGridItem from '../../components/renderGridItem';
 import renderListItem from '../../components/renderListItem';
 import Header from '../../components/Header';
 import FilterSortBar from '../../components/FilterSortBar';
+import SortModal from '../../components/SortModal';
 
 const AllProductsScreen = ({ navigation }) => {
   const [products, setProducts] = useState([]);
@@ -126,42 +127,6 @@ const AllProductsScreen = ({ navigation }) => {
     setFilteredProducts(products);
     setIsFilterModalVisible(false);
   };
-
-  const renderSortModal = () => (
-    <Modal
-      visible={isSortModalVisible}
-      transparent={true}
-      animationType="slide"
-      onRequestClose={() => setIsSortModalVisible(false)}
-    >
-      <TouchableWithoutFeedback onPress={() => setIsSortModalVisible(false)}>
-        <View className="flex-1 justify-end">
-          <View className="absolute top-0 left-0 right-0 bottom-0 bg-gray-800 opacity-50" />
-          <View className="bg-white p-4 rounded-t-xl border border-gray-300">
-            <Text className="text-lg font-bold mb-4">Sort By</Text>
-            <TouchableOpacity
-              className="p-2"
-              onPress={() => sortProducts('lowToHigh')}
-            >
-              <Text className="text-sm">Price: Low to High</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="p-2"
-              onPress={() => sortProducts('highToLow')}
-            >
-              <Text className="text-sm">Price: High to Low</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="p-2"
-              onPress={() => sortProducts('newest')}
-            >
-              <Text className="text-sm">Newest</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </Modal>
-  );
 
   const renderFilterModal = () => (
     <Modal
@@ -357,7 +322,11 @@ const AllProductsScreen = ({ navigation }) => {
           />
         )}
       </View>
-      {renderSortModal()}
+      <SortModal
+        isSortModalVisible={isSortModalVisible}
+        setIsSortModalVisible={setIsSortModalVisible}
+        sortProducts={sortProducts}
+      />
       {renderFilterModal()}
     </SafeAreaView>
   );
